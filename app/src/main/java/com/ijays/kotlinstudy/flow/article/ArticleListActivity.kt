2c5c516c7091b15d46bd17752c81c4e96f21e71c
songjie <ijaysdev@gmail.com>
2018-07-06 17:06:46 +0800

    Replace appbar listener with behavior

--- app/src/main/java/com/ijays/kotlinstudy/flow/article/ArticleListActivity.kt
+++ app/src/main/java/com/ijays/kotlinstudy/flow/article/ArticleListActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import com.bumptech.glide.Glide
 import com.ijays.kotlinstudy.R
@@ -40,12 +41,13 @@ class ArticleListActivity : BaseMvpActivity<ArticleListContract.View, ArticleLis
         // set immersive mode
         setImmersiveMode(window)
 
-        setRVListener()
-
         recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,
                 false)
         mAdapter = ArticleListAdapter(ArrayList())
         recycler_view.adapter = mAdapter
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         mPresenter.getArticleList(0)
 
@@ -87,48 +89,15 @@ class ArticleListActivity : BaseMvpActivity<ArticleListContract.View, ArticleLis
         }
     }
 
-    private fun setRVListener() {
 
-        ll_top_bar.post({
-            topBarHeight = ll_top_bar.measuredHeight
-        })
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return if (item?.itemId == android.R.id.home) {
+            finish()
+            true
 
-//        recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-//
-//            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
-//                super.onScrollStateChanged(recyclerView, newState)
-//            }
-//
-//            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
-//                super.onScrolled(recyclerView, dx, dy)
-//
-//                Log.e("SONGJIE", "---dy---$dy")
-//                Log.e("SONGJIE", "---topBarHeight---$topBarHeight")
-//                if (dy < topBarHeight) {
-//                    status_bar_view.alpha = (dy / topBarHeight).toFloat()
-//                }else{
-//                    status_bar_view.alpha=.95f
-//                }
-//            }
-//        })
-
-        app_bar.addOnOffsetChangedListener({ v, verticalOffset ->
-            Log.e("SONGJIE", "-offset is-->$verticalOffset--------->" + topBarHeight)
-
-            if (verticalOffset == 0) {
-                return@addOnOffsetChangedListener
-            }
-            if (abs(verticalOffset) < topBarHeight) {
-                Log.e("SONGJIE", "<<<<<<<")
-                status_bar_view.alpha = (abs(verticalOffset) / topBarHeight).toFloat()
-                toolbar_bg.alpha = (abs(verticalOffset) / topBarHeight).toFloat()
-            } else {
-                Log.e("SONGJIE", "======")
-                status_bar_view.alpha = 1f
-                toolbar_bg.alpha = 1f
-            }
-
-        })
+        } else {
+            super.onOptionsItemSelected(item)
+        }
     }
 
 
---
+++ app/src/main/java/com/ijays/kotlinstudy/view/behavior/TitleBarBehavior.kt
@@ -0,0 +1,38 @@
+package com.ijays.kotlinstudy.view.behavior
+
+import android.content.Context
+import android.support.design.widget.CoordinatorLayout
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+
+/**
+ * Created by ijays on 2018/7/6.
+ */
+class TitleBarBehavior @JvmOverloads constructor(context: Context, attributeSet: AttributeSet?)
+    : CoordinatorLayout.Behavior<View>(context, attributeSet) {
+
+    private var totalRange: Float = 0f
+
+    override fun layoutDependsOn(parent: CoordinatorLayout?, child: View?, dependency: View?): Boolean {
+        return dependency is RecyclerView
+    }
+
+    override fun onDependentViewChanged(parent: CoordinatorLayout?, child: View?, dependency: View?): Boolean {
+
+        if (totalRange == 0f) {
+            totalRange = dependency?.top?.toFloat() ?: 0f
+        }
+
+        val rate = 1 - (dependency?.top?.div(totalRange) ?: 0f)
+
+        Log.e("SONGJIE", "child height----->${child?.height}----->${dependency?.top}")
+
+        child?.alpha = rate
+
+        return true
+    }
+
+
+}
